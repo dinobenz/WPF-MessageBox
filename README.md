036b57a7a5d0a9ec36f65bd96ac298ce96220369
Pakorn Weecharungsan <dinobenz@hotmail.com>
2014-05-14 23:54:10 +0700

    initial commit
    create project

--- README.md
+++ README.md
@@ -1,4 +1,26 @@
 WPF-MessageBox
 ==============
 
-WPF MessageBox
+Custom WPF MessageBox
+
+Usage
+==============
+Display message text
+```
+MessageBoxWindow.Show(this, "Put your message here.");
+```
+
+Display message text and caption
+```
+MessageBoxWindow.Show(this, "Put your message here.", "Put your caption here");
+```
+
+Display message text, caption and Yes/No button
+```
+MessageBoxResult msgResult = MessageBoxWindow.Show(this, "Put your message here.", "Put your caption here", MessageBoxButton.YesNo);
+```
+
+Display message text, caption, button and icon
+```
+MessageBoxWindow.Show(this, "Put your message here.", "Put your caption here", MessageBoxButton.OK, MessageBoxImage.Warning);
+```
---
+++ src/WPF-MessageBox/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Dinobenz.Wpf.MessageBox.View;
+
+namespace Dinobenz.Wpf.MessageBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Message_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxWindow.Show(this, "Put your message here.");
+        }
+
+        private void Message_Caption_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxWindow.Show(this, "Put your message here.", "Put your caption here");
+        }
+
+        private void Message_Caption_Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult msgResult = MessageBoxWindow.Show(this, "Put your message here.", "Put your caption here", MessageBoxButton.YesNo);
+        }
+
+        private void Message_Caption_Button_Icon_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBoxWindow.Show(this, "Put your message here.", "Put your caption here", MessageBoxButton.OK, MessageBoxImage.Warning);
+        }
+    }
+}
